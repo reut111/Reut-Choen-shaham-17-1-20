50cf87538850499d4a70fd6376768acde6655962
unknown <reutic@gmail.com>
2020-01-23 16:01:20 +0200

    Auto-generated commit

--- shared/weather.ts
+++
@@ -1,9 +0,0 @@
-export class Weather {
-    currWeather: any;
-    locationKey: string;
-    fivaDaysforecasts: any;
-    cityName:string;
-    temperature: any;
-    unit:string;
-    weatherText: string;
-}
